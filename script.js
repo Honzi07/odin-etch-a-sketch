fb4948fa3a23ec7a04a6f727250afb5516f4d64c
Honzi07 <nyisztor.zsolt@gmail.com>
2022-08-05 04:28:08 +0200

    added color selector and some html element for the future

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 const container = document.querySelector('#container')
+const square = document.querySelectorAll('.square')
+const colorPicker = document.querySelector('#colorPicker')
+const resetBtn = document.querySelector('#resetBtn')
+
+let color = undefined;
 
 function createSquare() {
     const square = document.createElement('div')
     square.className = 'square';
     container.appendChild(square);
+    square.addEventListener('mouseover', 
+    event => event.target.style.backgroundColor = color)
 }
 
 function createDrawingBoard(squareNum) {
     container.style.gridTemplateColumns = `repeat(${squareNum}, 1fr [col-start])`;
-    const total = squareNum * squareNum;
-    for(i = 0; i < total; i++) {
+    container.style.gridTemplateRows = `repeat(${squareNum}, 1fr [col-start])`;
+    for(i = 0; i < (squareNum * squareNum); i++) {
         createSquare()
     }
 }
+
+
+
+colorPicker.addEventListener('change', function(event){
+    color = event.target.value;
+})
+
+
+createDrawingBoard(16)
+
+
+
