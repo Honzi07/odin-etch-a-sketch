5abcbaf4f7caca9249b26de8387538f8f3c686f2
Honzi07 <nyisztor.zsolt@gmail.com>
2022-08-12 01:24:42 +0200

    button for creating new grid

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const container = document.querySelector('#container')
-const square = document.querySelectorAll('.square')
-const colorPicker = document.querySelector('#colorPicker')
-const resetBtn = document.querySelector('#resetBtn')
+const container = document.querySelector('#container');
+const square = document.querySelectorAll('.square');
+const colorPicker = document.querySelector('#colorPicker');
+const resetBtn = document.querySelector('#resetBtn');
 
 
 let color = undefined;
+
 
 function createSquare() {
     const square = document.createElement('div')
@@ -18,7 +19,7 @@ function createDrawingBoard(squareNum) {
     container.style.gridTemplateColumns = `repeat(${squareNum}, 1fr [col-start])`;
     container.style.gridTemplateRows = `repeat(${squareNum}, 1fr [col-start])`;
     for(i = 0; i < (squareNum * squareNum); i++) {
-        createSquare()
+        createSquare();
     }
 }
 
@@ -27,17 +28,31 @@ colorPicker.addEventListener('change', function(event){
 })
 
 
-function num() {
+function newGrid() {
     do{
         squareNum = prompt('Enter a number between 16 and 100')
     }
-    while(squareNum < 16 || squareNum > 100);
-
-    console.log(squareNum)
-    createDrawingBoard(squareNum)    
+    while(squareNum < 16 || squareNum > 100);    
+    createDrawingBoard(squareNum);   
 }
 
+// function remove() {
+//     let child = container.lastElementChild; 
+//         while (child) {
+//             container.removeChild(child);
+//             child = container.lastElementChild;
+//         }
+// }
 
-num()
+resetBtn.addEventListener('click', () =>{
+    // remove()
+    colorPicker.value = '#ffffff';
+    color = '#ffffff';
+    container.innerHTML = '';
+    newGrid();
+});
+
+createDrawingBoard(16)
+
 
 
